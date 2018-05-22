e2ca84352becbdc92b31d2a97a7936cdc148c105
fuzznroll <36281574+fuzznroll@users.noreply.github.com>
2018-05-22 22:29:40 +0200

    Initial commit

---
+++ AR_Soundinstallation/Assets/Script/InTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class InTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter Trigger");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit Trigger");
+    }
+}
