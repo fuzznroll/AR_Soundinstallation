7a69172f9b2292c304a5af63c65e736d54bb8208
fuzznroll <36281574+fuzznroll@users.noreply.github.com>
2018-05-24 17:21:27 +0200

    Wwise State implementation in Unity for Trigger
    -states for in and out at trigger setted
    -in and out cues setted

--- AR_Soundinstallation/Assets/Script/InTrigger.cs
+++ AR_Soundinstallation/Assets/Script/InTrigger.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 
+
 public class InTrigger : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Enter Trigger");
+        AkSoundEngine.PostEvent("Music", gameObject);
+
+        Debug.Log("Enter Trigger"); 
+        AkSoundEngine.SetState("Music_State", "In");
     }
 
     private void OnTriggerExit(Collider other)
     {
         Debug.Log("Exit Trigger");
+        AkSoundEngine.SetState("Music_State", "Out");
     }
 }
---
+++ AR_Soundinstallation/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,63 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace STATES
+    {
+        namespace MUSIC_STATE
+        {
+            static const AkUniqueID GROUP = 3826569560U;
+
+            namespace STATE
+            {
+                static const AkUniqueID IN = 1752637612U;
+                static const AkUniqueID OUT = 645492555U;
+            } // namespace STATE
+        } // namespace MUSIC_STATE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace OUTIN
+        {
+            static const AkUniqueID GROUP = 242156934U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID IN = 1752637612U;
+                static const AkUniqueID OUT = 645492555U;
+            } // namespace SWITCH
+        } // namespace OUTIN
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK = 1661994096U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
